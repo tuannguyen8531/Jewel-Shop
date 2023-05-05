befd4057f75b659b43e8d7b57634a8f37d2b96d4
Anh Tuấn <tuannguyen8531@gmail.com>
2023-05-05 09:21:47 +0700

    Add files.

---
+++ android/app/src/main/java/vn/edu/ntu/project/jewel_project/MainActivity.java
@@ -0,0 +1,6 @@
+package vn.edu.ntu.project.jewel_project;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/firebase/widget_connect_firebase.dart
@@ -0,0 +1,83 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class MyFirebaseConnection extends StatefulWidget {
+  final String? errorMessage;
+  final String? connectingMessage;
+  final Widget Function(BuildContext context)? builder;
+  const MyFirebaseConnection({
+    Key? key,
+    required this.errorMessage,
+    required this.connectingMessage,
+    required this.builder
+  }) : super(key: key);
+
+  @override
+  State<MyFirebaseConnection> createState() => _MyFirebaseConnectionState();
+}
+
+class _MyFirebaseConnectionState extends State<MyFirebaseConnection> {
+  bool isConnected = false;
+  bool isError = false;
+  @override
+  Widget build(BuildContext context) {
+    if(isError) {
+      return Center(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            widget.errorMessage!,
+            style: const TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      );
+    }
+    else if(!isConnected) {
+      return Center(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircularProgressIndicator(),
+              Text(
+                widget.connectingMessage!,
+                style: const TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+    else {
+      return widget.builder!(context);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _khoitaoFirebase();
+  }
+
+  _khoitaoFirebase() {
+    Firebase.initializeApp()
+        .then((value) { //try
+          setState(() {
+            isConnected = true;
+          });
+        })
+        .catchError((error) { //catch
+          setState(() {
+            isError = true;
+          });
+        })
+        .whenComplete(() => print("Init completed.")); //finally
+  }
+}
