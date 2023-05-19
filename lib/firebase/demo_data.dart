3fd28ff1cb20aa920c8e047ccbef54ffa28970f7
Anh Tuấn <tuannguyen8531@gmail.com>
2023-05-19 12:32:51 +0700

    Update.

--- lib/firebase/demo_data.dart
+++ lib/firebase/demo_data.dart
@@ -14,3 +14,41 @@ class GemType {
   VoidCallback press;
   GemType({required this.title, required this.icon, required this.press});
 }
+
+class Gemstone {
+  String image, name, price;
+  Gemstone({required this.image, required this.name, required this.price});
+}
+
+List<Gemstone> gemstones = [
+  Gemstone(
+      image:
+          "https://firebasestorage.googleapis.com/v0/b/jewel-project-17b95.appspot.com/o/Imagines%2FBlack%20Diamond.jpg?alt=media&token=7dc67906-f06f-4134-9bae-debf6503028e",
+      name: "Black Diamond",
+      price: "15000"),
+  Gemstone(
+      image:
+          "https://firebasestorage.googleapis.com/v0/b/jewel-project-17b95.appspot.com/o/Imagines%2FHydrothermal%20Emerald.jpg?alt=media&token=8b3b7683-72b4-4e7a-b3e2-72a045278fe9",
+      name: "Hydrothermal Emerald",
+      price: "50000"),
+  Gemstone(
+      image:
+          "https://firebasestorage.googleapis.com/v0/b/jewel-project-17b95.appspot.com/o/Imagines%2FCeylon%20Sapphire.jpg?alt=media&token=d0550d18-f7ba-4c6a-a806-5b849fd1a2b5",
+      name: "Ceylon Sapphire",
+      price: "22000"),
+  Gemstone(
+      image:
+          "https://firebasestorage.googleapis.com/v0/b/jewel-project-17b95.appspot.com/o/Imagines%2FBaltic%20Amber.jpg?alt=media&token=ab0f1930-a203-4920-8762-47be1af892af",
+      name: "Baltic Amber",
+      price: "17000"),
+  Gemstone(
+      image:
+          "https://firebasestorage.googleapis.com/v0/b/jewel-project-17b95.appspot.com/o/Imagines%2FPigeon%20Blood%20Ruby.jpg?alt=media&token=26b4cc5f-fec4-4b73-8b02-89d1efe83b6f",
+      name: "Pigeon Blood",
+      price: "9000"),
+  Gemstone(
+      image:
+          "https://firebasestorage.googleapis.com/v0/b/jewel-project-17b95.appspot.com/o/Imagines%2FStar%20Ruby.jpg?alt=media&token=7c9db616-75ea-4fd8-893d-e8a94f7124d4",
+      name: "Star Ruby",
+      price: "37000"),
+];
--- lib/firebase/pagehome.dart
+++ lib/firebase/pagehome.dart
@@ -49,70 +49,189 @@ class PageHome extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 10),
-            const Text(
-              "Explore our gemstones",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              const Text(
+                "Explore our gemstones",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const Text("we choose the best for you..."),
-            const SizedBox(height: 25),
-            Form(
-              key: formState,
-              autovalidateMode: AutovalidateMode.disabled,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: txtSearch,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              const Text("we choose the best for you..."),
+              const SizedBox(height: 25),
+              Form(
+                key: formState,
+                autovalidateMode: AutovalidateMode.disabled,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: txtSearch,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        ),
+                        hintText: "Search everything...",
+                        prefixIcon: const Icon(Icons.search),
+                        suffixIcon: IconButton(
+                          onPressed: () => txtSearch.clear(),
+                          icon: const Icon(Icons.clear),
+                        ),
                       ),
-                      hintText: "Search everything...",
-                      prefixIcon: const Icon(Icons.search),
-                      suffixIcon: IconButton(
-                        onPressed: () => txtSearch.clear(),
-                        icon: const Icon(Icons.clear),
-                      ),
+                      validator: (value) {
+                        return value!.isEmpty ? "Nothing to search..." : null;
+                      },
                     ),
-                    validator: (value) {
-                      return value!.isEmpty ? "Nothing to search..." : null;
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 25),
-            const Text(
-              "Category",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
+              const SizedBox(height: 25),
+              const Text(
+                "Category",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List.generate(
-                  gems.length,
-                  (index) => Padding(
-                    padding: const EdgeInsets.all(6.0),
-                    child: CategoryCard(
-                      icon: gems[index].icon,
-                      type: gems[index].title,
-                      press: gems[index].press,
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                    gems.length,
+                    (index) => Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: CategoryCard(
+                        icon: gems[index].icon,
+                        type: gems[index].title,
+                        press: gems[index].press,
+                      ),
                     ),
                   ),
                 ),
               ),
+              const SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Our product",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "See all",
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ProductCard(
+                    name: gemstones[0].name,
+                    price: gemstones[0].price,
+                    image: gemstones[0].image,
+                    press: () {
+                      txtSearch.text = gemstones[0].name;
+                    },
+                  ),
+                  ProductCard(
+                    name: gemstones[1].name,
+                    price: gemstones[1].price,
+                    image: gemstones[1].image,
+                    press: () {
+                      txtSearch.text = gemstones[1].name;
+                    },
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ProductCard(
+                    name: gemstones[2].name,
+                    price: gemstones[2].price,
+                    image: gemstones[2].image,
+                    press: () {
+                      txtSearch.text = gemstones[2].name;
+                    },
+                  ),
+                  ProductCard(
+                    name: gemstones[3].name,
+                    price: gemstones[3].price,
+                    image: gemstones[3].image,
+                    press: () {
+                      txtSearch.text = gemstones[3].name;
+                    },
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ProductCard(
+                    name: gemstones[4].name,
+                    price: gemstones[4].price,
+                    image: gemstones[4].image,
+                    press: () {
+                      txtSearch.text = gemstones[4].name;
+                    },
+                  ),
+                  ProductCard(
+                    name: gemstones[5].name,
+                    price: gemstones[5].price,
+                    image: gemstones[5].image,
+                    press: () {
+                      txtSearch.text = gemstones[5].name;
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  ProductCard({
+    Key? key,
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.press,
+  }) : super(key: key);
+  String name, price, image;
+  VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Card(
+        elevation: 1,
+        shadowColor: Colors.blue,
+        child: Column(
+          children: [
+            Image.network(image, height: 180,),
+            Text(name),
+            Text(
+              "\$$price",
+              style: const TextStyle(color: Colors.red),
             ),
           ],
         ),
@@ -148,7 +267,7 @@ class CategoryCard extends StatelessWidget {
               const SizedBox(height: 5),
               Text(
                 type,
-                style: TextStyle(color: Colors.amber),
+                style: const TextStyle(color: Colors.amber),
               ),
             ],
           ),
@@ -157,3 +276,4 @@ class CategoryCard extends StatelessWidget {
     );
   }
 }
+
